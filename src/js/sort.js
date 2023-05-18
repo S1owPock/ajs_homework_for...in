2b8480ff96dc57081a33da538783470b5161e85b
Max Solodovnikov <maks_solod@list.ru>
2023-05-18 14:24:49 +0300

    тест

--- src/js/sort.js
+++ src/js/sort.js
@@ -6,24 +6,24 @@ export default function sortingByParam(obj, arr) {
   };
   Object.setPrototypeOf(copyObj, objectProto);
   const tempArr = [];
-  
+
   arr.forEach((elem) => {
     if (Object.prototype.hasOwnProperty.call(copyObj, elem)) {
       ordedObj.push({ key: elem, value: obj[elem] });
       delete copyObj[elem];
     }
   });
-  
+
   for (const key in copyObj) {
     if (Object.prototype.hasOwnProperty.call(copyObj, key)) {
       tempArr.push({ key, value: copyObj[key] });
     }
   }
-  
+
   tempArr.sort((prev, next) => {
     if (prev.key < next.key) return -1;
       return '';
   });
-  
+
   return ordedObj.concat(tempArr);
 }
